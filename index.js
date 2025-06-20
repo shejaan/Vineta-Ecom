8796e7a860886295b13511e9ba5c19f6345fed3a
Shejaan <shejank84@gmail.com>
2025-06-20 16:57:59 +0530

    Initial commit 2

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require('path');
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req,res)=>{
-    res.render('home-electronic.ejs');
+    res.render('index.ejs');
 });
+
+app.get('/shop-sub-collection', (req,res)=>{
+    res.render('shop-sub-collection.ejs')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
